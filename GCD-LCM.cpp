8a7aa8f67d578d10a1b5e2b65f8d04edc9ce7d12
3tacy <101511712+MyersWardenBlack@users.noreply.github.com>
2023-08-04 14:32:17 +0800

    Update GCD-LCM.cpp

--- GCD-LCM.cpp
+++ GCD-LCM.cpp
@@ -1,25 +1,25 @@
-//
+//《算法训练营（入门篇）》-陈小玉
 
 
 //1-29
 #include <iostream>
 using namespace std;
 
-int GCD(int x, int y){
+int GCD(int x, int y){         //最大公因数GCD
    int t;
    t = x % y;
    while(t != 0){
       x = y;
-      y = t;
+      y = t;               //交换
       t = x % y;
    }
    return y;
 }
 
-int LCM(int x, int y){
+int LCM(int x, int y){         //最小公倍数LCM
    int g;
    g = GCD(x, y);
-   return (x * y / g);
+   return (x * y / g);         //LCM = 两个数相乘除以最大公因数
 }
 
 int main()
